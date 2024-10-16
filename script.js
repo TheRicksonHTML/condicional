d4c90bd36cd5da015f103c009406e7ca704341da
TheRicksonHTMl <ricksonnuneserick@gmail.com>
2024-10-16 15:42:35 -0300

    condicional

---
+++ script.js
@@ -0,0 +1,27 @@
+// sao uma forma de tomar decisoes, de fazer o computador executar essas decisoes. 
+
+// if significa "se"
+// else significa "senão"
+
+// if (condeção) {codigo que sera executado}
+
+//pronmpt pergunta algo ao usuario;
+
+// variaveis
+//3 var, let e const 
+// prompt pergunta algo ao usuario;
+//sempre que usamos dados textuais ""
+// string
+let nome = prompt ("qual é o seu nome?");
+
+//alert usamos para exibir uma mensagem
+
+alert ("seja bem vindo (a) " + nome);
+
+let idade = prompt ("Quantos anos voce tem?")
+
+if (idade >= 18) {alert ("você ja pode tirar a carteira de motorista!")} else { alert ("voce nao tem idade para tirar a carteira") }
+
+let gostarDePizza = (" voce gosta de pizza? responda (sim ou nao)")
+
+if (gostarDePizza === "sim"){alert ("que otimo! vamos comer juntos!")} else { alert ("tudo bem, talvez voce goste de outra comida")}
